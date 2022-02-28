0ce8b755c1b89fde6e88b150d0f90bb53497f98a
Bruna Reveriego <Bruna.Reveriego@br.ey.com>
2022-02-28 18:35:38 -0300

    criacao teste de integracao end to end

---
+++ API/StartupApiTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesteIntegracao.Domain;
+
+namespace API
+{
+    public class StartupApiTests
+    {
+        public StartupApiTests(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ICaixa, Caixa>();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            //OR
+            services.AddControllers(options => options.EnableEndpointRouting = false);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            app.UseMvc();
+        }
+
+    }
+}
